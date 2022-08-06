8fde5cf5c0a56d88eef6f1fab2a32a7a6fd281a7
willywes <willywes@hotmail.cl>
2022-08-06 18:57:35 -0400

    [ADDED] Configs

--- next.config.js
+++ next.config.js
@@ -2,6 +2,21 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  i18n: {
+    locales: ['en', 'sr', 'es', 'pseudo'],
+    pseudoLocale: 'pseudo',
+    sourceLocale: 'en',
+    fallbackLocales: {
+      default: 'en'
+    },
+    catalogs: [
+      {
+        path: 'src/translations/locales/{locale}/messages',
+        include: ['src/pages', 'src/components']
+      }
+    ],
+    format: 'po'
+  }
 }
 
 module.exports = nextConfig
