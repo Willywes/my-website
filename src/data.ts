e92565c5126bc2d950f4059f9c44ea078c2ea1d3
willywes <willywes@hotmail.cl>
2023-01-05 20:10:08 -0300

    [ADDED] Datas

--- src/data.ts
+++ src/data.ts
@@ -1,4 +1,4 @@
-import {Job} from "./types";
+import {Certification, Education, Job} from "./types";
 
 export const jobs: Job[] = [
     {
@@ -93,3 +93,69 @@ export const jobs: Job[] = [
         description: 'Como Desarrollador Web y Móvil en Want Agencia de Diseño, una empresa de diseño y tecnología con sede en Viña del Mar, Chile, tuve tenido la oportunidad de aplicar mis conocimientos y habilidades en proyectos tanto para la web como para dispositivos móviles utilizando diversas tecnologías como Laravel, Wordpress, React.js, React Native y Angular.'
     }
 ];
+
+export const educations : Education[] = [
+    {
+        title: 'Ingeniería en Informática',
+        institution: {
+            name: 'Duoc UC',
+            url: 'https://duoc.cl',
+        },
+        location: 'Chile, Viña del Mar',
+        period: {
+            from: new Date(2019, 12, 1),
+            to: new Date(2015, 3, 1),
+        }
+    },
+    {
+        title: 'Analista Programador Computacional',
+        institution: {
+            name: 'Duoc UC',
+            url: 'https://duoc.cl',
+        },
+        location: 'Chile, Viña del Mar',
+        period: {
+            from: new Date(2017, 12, 1),
+            to: new Date(2015, 3, 1),
+        }
+    }
+]
+
+export const certifications : Certification[] = [
+    {
+        title: 'Scrum Master Certified Expert',
+        institution: {
+            name: 'Certjoin',
+            url: 'https://certjoin.com/campus/mod/customcert/verify_certificate.php',
+            code : 'nkFh5hyPX5'
+        },
+        date: new Date(2021, 11, 1),
+    },
+    {
+        title: 'Scrum Product Owner Certified Expert',
+        institution: {
+            name: 'Certjoin',
+            url: 'https://certjoin.com/campus/mod/customcert/verify_certificate.php',
+            code : 'MQ4F0FrkMe'
+        },
+        date: new Date(2021, 11, 1),
+    },
+    {
+        title: 'Desarrollo de App Móviles',
+        institution: {
+            name: 'Google Actívate',
+            url: 'https://learndigital.withgoogle.com/activate/validate-certificate-code',
+            code : 'ETK UEM LUB'
+        },
+        date: new Date(2021, 4, 1),
+    },
+    {
+        title: 'Programación en Python I',
+        institution: {
+            name: 'Coursera',
+            url: 'https://www.coursera.org/account/accomplishments/verify/TLLTAKHH6Q5F',
+            code : 'TLLTAKHH6Q5F'
+        },
+        date: new Date(2020, 11, 1),
+    }
+]
--- src/types.ts
+++ src/types.ts
@@ -12,3 +12,33 @@ export type Job = {
     aptitudes: string[];
     description: string;
 }
+
+export type Education = {
+    title: string;
+    institution: {
+        name: string;
+        url: string;
+    },
+    location: string;
+    period: {
+        from: Date;
+        to: Date | null;
+    }
+    description?: string;
+}
+
+export type Certification = {
+    title: string;
+    institution: {
+        name: string;
+        url: string;
+        code?: string;
+    },
+    date: Date;
+    description?: string;
+}
+
+export type Skill = {
+    name: string;
+    level: number;
+}
