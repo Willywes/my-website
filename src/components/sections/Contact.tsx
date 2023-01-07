153e3daf65d4fedbed4c1f0c9796b3573657a48b
willywes <willywes@hotmail.cl>
2023-01-07 00:01:26 -0300

    contact form

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -1,50 +1,85 @@
 import React, { ChangeEvent, useState } from 'react';
+import { BsCheck2Circle } from 'react-icons/bs';
+import { BiErrorCircle } from 'react-icons/bi';
 
+const defaultFormState = {
+    name: '',
+    email: '',
+    message: ''
+};
 const Contact = () => {
-    const [data, setData] = useState({
-        name: '',
-        email: '',
-        message: ''
-    });
-
+    const [data, setData] = useState(defaultFormState);
     const [sending, setSending] = useState(false);
+    const [success, setSuccess] = useState(false);
+    const [error, setError] = useState(false);
+    const [errorFields, setErrorFields] = useState(false);
 
     const handleInputChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        setData({
-            ...data,
-            [e.target.name]: e.target.value
-        });
+        setData({ ...data, [e.target.name]: e.target.value });
+        if (success) setSuccess(false);
+        if (error) setError(false);
+        if (errorFields) setErrorFields(false);
     };
 
     const handleSubmit = async () => {
         setSending(true);
+        try {
+            const { name, email, message } = data;
 
-        const { name, email, message } = data;
+            if (!name || !email || !message) {
+                setErrorFields(true);
+                setSending(false);
+                return;
+            }
 
-        const res = await fetch('/api/sengrid', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({ name, email, message })
-        });
-
-        const json = await res.json();
-
-        console.log(json);
-
-        // if (json.error) {
-        //
-        // } else {
-        //
-        // }
-
+            await fetch('/api/sengrid', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ name, email, message })
+            })
+                .then((res) => {
+                    console.log(res.status);
+                    if (res.status === 200) {
+                        runSuccess();
+                    } else {
+                        runError();
+                    }
+                })
+                .finally(() => setSending(false));
+        } catch (err) {
+            runError();
+        }
         setSending(false);
     };
+    const runSuccess = () => {
+        setData(defaultFormState);
+        setSuccess(true);
+    };
 
+    const runError = () => {
+        setError(true);
+    };
     return (
         <>
             <h2>Hablemos</h2>
+
+            {success === true && (
+                <div className="alert alert-success">
+                    <span className="icon-success">
+                        <BsCheck2Circle />
+                    </span>{' '}
+                    Gracias por contactarte conmigo, te responderé a la brevedad.
+                </div>
+            )}
+
+            {error === true && (
+                <div className="alert alert-danger">
+                    <span className="icon-danger">
+                        <BiErrorCircle />
+                    </span>{' '}
+                    Hubo un error al enviar el mensaje, por favor intenta nuevamente.
+                </div>
+            )}
             <div className="mb-3">
                 <label htmlFor="name" className="form-label">
                     Nombre
@@ -54,8 +89,12 @@ const Contact = () => {
                     className={`form-control ${sending ? 'disabled' : ''}`}
                     id="name"
                     name="name"
+                    value={data.name}
                     onChange={handleInputChange}
                 />
+                {errorFields && !data.name && (
+                    <small className="text-danger">El campos nombre es requerido</small>
+                )}
             </div>
             <div className="mb-3">
                 <label htmlFor="email" className="form-label">
@@ -66,8 +105,12 @@ const Contact = () => {
                     className={`form-control ${sending ? 'disabled' : ''}`}
                     id="email"
                     name="email"
+                    value={data.email}
                     onChange={handleInputChange}
                 />
+                {errorFields && !data.email && (
+                    <small className="text-danger">El campos email es requerido</small>
+                )}
             </div>
             <div className="mb-3">
                 <label htmlFor="message" className="form-label">
@@ -77,12 +120,18 @@ const Contact = () => {
                     className={`form-control ${sending ? 'disabled' : ''}`}
                     id="message"
                     name="message"
-                    rows={3}
+                    rows={4}
+                    style={{ resize: 'none' }}
+                    value={data.message}
                     onChange={handleInputChange}
                 ></textarea>
+                {errorFields && !data.message && (
+                    <small className="text-danger">El campos mensaje es requerido</small>
+                )}
             </div>
+
             {sending ? (
-                <button className="btn btn-primary" type="button" disabled>
+                <button className="btn btn-form mt-3" type="button" disabled>
                     <span
                         className="spinner-border spinner-border-sm"
                         role="status"
@@ -91,12 +140,11 @@ const Contact = () => {
                     Enviando...
                 </button>
             ) : (
-                <button className="btn btn-primary" type="button" onClick={handleSubmit}>
+                <button className="btn btn-form mt-3 px-5" type="button" onClick={handleSubmit}>
                     Enviar
                 </button>
             )}
         </>
     );
 };
-
 export default Contact;
