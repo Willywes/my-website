f4de78b59cde0dc544f11eff0ad0d9c62c26bd42
willywes <willywes@hotmail.cl>
2023-01-17 16:15:49 -0300

    CSS Styling change

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import '../assets/scss/theme.scss';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { APP_CONFIG } from '../config';
+import { GoogleReCaptchaProvider } from 'react-google-recaptcha-v3';
 
 function App({ Component, pageProps }: AppProps) {
     return (
@@ -145,7 +146,17 @@ function App({ Component, pageProps }: AppProps) {
                     }}
                 />
             </Head>
-            <Component {...pageProps} />
+            <GoogleReCaptchaProvider
+                reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY ?? ''}
+                scriptProps={{
+                    async: false, // optional, default to false,
+                    defer: true, // optional, default to false
+                    appendTo: 'body', // optional, default to "head", can be "head" or "body",
+                    nonce: undefined
+                }}
+            >
+                <Component {...pageProps} />
+            </GoogleReCaptchaProvider>
         </>
     );
 }
